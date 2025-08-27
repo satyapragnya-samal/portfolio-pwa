d08610f04dbde4a43844f2b615e95042a1853769
satyapragnya-samal <satyapragnyasamal82@gmail.com>
2025-08-27 19:58:27 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,33 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("portfolio-cache").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./WhatsApp Image 2025-07-30 at 10.14.52_f4f65052.jpg"
-      ]);
-    })
+// sw.js
+const CACHE_NAME = "portfolio-cache-v2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./WhatsApp Image 2025-07-30 at 10.14.52_f4f65052.jpg"
+];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting(); // activate this SW immediately after install
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
+self.addEventListener("activate", (event) => {
+  // clean old caches
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+  return self.clients.claim(); // take control of open pages
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
   );
 });
